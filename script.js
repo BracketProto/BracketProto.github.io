575bbabe87b2dd5c1623dbd487b2c5884b49c061
BracketProto <creepertruck812@gmail.com>
2022-06-24 20:30:32 +0100

    e

--- script.js
+++ script.js
@@ -45,9 +45,8 @@ readTextFile("./events.json", function (text) {
   }
 });
 
-var i = 0;
 function update() {
-  for (i < events.events[0].event.Length; i++;) {
+  for (i = 0; i < events.events[0].event.length; i++) {
       /*#__PURE__*/
       React.createElement("h1", null, events.events[0].event[i].name),
       /*#__PURE__*/
