dc7bf1c3d92a79e1007b4667344c423cfa2d2f5f
BracketProto <69521768+OCSYT@users.noreply.github.com>
2023-02-07 11:26:18 +0000

    remove useless comments lol

--- script.js
+++ script.js
@@ -10,66 +10,66 @@ var subtitle = "{PROJBRCKT V" + version + " | BracketProto.com }";
 
 function update() {
   value = value + 1;
-  var element = /*#__PURE__*/ React.createElement(
+  var element =React.createElement(
     "div",
     null,
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "div",
       {
         class: "headertext"
       },
-      /*#__PURE__*/ React.createElement("h1", null, title)
+     React.createElement("h1", null, title)
     ),
-    /*#__PURE__*/ React.createElement("br", null),
-    /*#__PURE__*/ React.createElement(
+   React.createElement("br", null),
+   React.createElement(
       "div",
       {
         class: "smalltext"
       },
-      /*#__PURE__*/ React.createElement("p", null, subtitle)
+     React.createElement("p", null, subtitle)
     ),
-    /*#__PURE__*/ React.createElement("br", null),
-    /*#__PURE__*/ React.createElement("iframe", {
+   React.createElement("br", null),
+   React.createElement("iframe", {
       src: "https://lanyard.cnrad.dev/api/530036821707718678",
       frameBorder: "0",
       height: "250px",
       width: "450px"
     }),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "div",
       {
         class: "sameline"
       },
-      /*#__PURE__*/ React.createElement("h2", null, "About me"),
-      /*#__PURE__*/ React.createElement("p", null, "Hi, I'm Bracket!"),
-      /*#__PURE__*/ React.createElement(
+     React.createElement("h2", null, "About me"),
+     React.createElement("p", null, "Hi, I'm Bracket!"),
+     React.createElement(
         "p",
         null,
         "I'm a furry from the UK that makes some content"
       ),
-      /*#__PURE__*/ React.createElement(
+     React.createElement(
         "p",
         null,
         "and makes projects such as websites, games, and discord bots!"
       ),
-      /*#__PURE__*/ React.createElement("img", {
+     React.createElement("img", {
         class: "imgclass2",
         src: "./img/BracketSit.png"
       })
     ),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "h1",
       null,
       "This page is made using the React JavaScript library with HTML and CSS"
     ),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "h2",
       null,
       "It has been ",
       value,
       " seconds since you opened this page"
     ),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "a",
       {
         class: "btn",
@@ -77,7 +77,7 @@ function update() {
       },
       "\u2800React Documentation\u2800"
     ),
-    /*#__PURE__*/ React.createElement("br", null),
+   React.createElement("br", null),
 	/*#__PURE__*/ React.createElement("br", null),
 	/*#__PURE__*/ React.createElement("br", null),
 	/*#__PURE__*/ React.createElement("br", null),
@@ -88,31 +88,31 @@ function update() {
 	/*#__PURE__*/ React.createElement("br", null),
 	/*#__PURE__*/ React.createElement("br", null),
 	/*#__PURE__*/ React.createElement("br", null),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "div",
       {
         class: "section"
       },
-      /*#__PURE__*/ React.createElement(
+     React.createElement(
         "h1",
         null,
         "Bracket Proto - References"
       ),
-      /*#__PURE__*/ React.createElement("img", {
+     React.createElement("img", {
         width: "30%",
         src: "./img/ref.png"
       }),
-	        /*#__PURE__*/ React.createElement("img", {
+	       React.createElement("img", {
         width: "30%",
         src: "./img/bracket3d.png"
       }),
-      /*#__PURE__*/ React.createElement(
+     React.createElement(
         "div",
         {
           class: "sketchfab-embed-wrapper"
         },
         " ",
-        /*#__PURE__*/ React.createElement(
+       React.createElement(
           "iframe",
           {
             title: "NKD Protogen 3_0",
@@ -132,17 +132,17 @@ function update() {
           " "
         ),
         " ",
-        /*#__PURE__*/ React.createElement("p", {})
+       React.createElement("p", {})
       )
     ),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "div",
       {
         class: "section2"
       },
-      /*#__PURE__*/ React.createElement("br", null),
-      /*#__PURE__*/ React.createElement("h1", null, "Discord"),
-      /*#__PURE__*/ React.createElement(
+     React.createElement("br", null),
+     React.createElement("h1", null, "Discord"),
+     React.createElement(
         "a",
         {
           class: "btn",
@@ -150,10 +150,10 @@ function update() {
         },
         "\u2800Join the community!\u2800"
       ),
-      /*#__PURE__*/ React.createElement("br", null),
-      /*#__PURE__*/ React.createElement("br", null),
-      /*#__PURE__*/ React.createElement("br", null),
-      /*#__PURE__*/ React.createElement(
+     React.createElement("br", null),
+     React.createElement("br", null),
+     React.createElement("br", null),
+     React.createElement(
         "widgetbot",
         {
           server: "929955650237251594",
@@ -161,7 +161,7 @@ function update() {
           width: "100%",
           height: "500"
         },
-        /*#__PURE__*/ React.createElement("iframe", {
+       React.createElement("iframe", {
           title: "WidgetBot Discord chat embed",
           src: "https://e.widgetbot.io/channels/929955650237251594/929955830911086673/?api=db66bb54-dc8b-424f-b181-6a6d6459ff5f",
           frameBorder: "0",
@@ -170,15 +170,15 @@ function update() {
         })
       )
     ),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "div",
       {
         class: "section3"
       },
-      /*#__PURE__*/ React.createElement("br", null),
-      /*#__PURE__*/ React.createElement("h1", null, "Twitch"),
-      /*#__PURE__*/ React.createElement("br", null),
-      /*#__PURE__*/ React.createElement("iframe", {
+     React.createElement("br", null),
+     React.createElement("h1", null, "Twitch"),
+     React.createElement("br", null),
+     React.createElement("iframe", {
         src: "https://embed.twitch.tv?autoplay=true&channel=bracketproto&height=480&layout=video&parent=bracketproto.com&referrer=https%3A%2F%2Fbracketproto.com%2F&width=854",
         allowfullscreen: "",
         scrolling: "no",
@@ -191,62 +191,62 @@ function update() {
         height: "480"
       })
     ),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "div",
       {
         class: "section4",
         id: "4"
       },
-      /*#__PURE__*/ React.createElement("br", null),
-      /*#__PURE__*/ React.createElement("h1", null, "Donate"),
-      /*#__PURE__*/ React.createElement("br", null)
+     React.createElement("br", null),
+     React.createElement("h1", null, "Donate"),
+     React.createElement("br", null)
       /*#__PURE__*/
     ),
-    /*#__PURE__*/ React.createElement(
+   React.createElement(
       "div",
       {
         class: "section5"
       },
-      /*#__PURE__*/ React.createElement("h1", null, "Other Links"),
-      /*#__PURE__*/ React.createElement(
+     React.createElement("h1", null, "Other Links"),
+     React.createElement(
         "a",
         {
           href: "https://twitch.tv/bracketproto"
         },
-        /*#__PURE__*/ React.createElement("img", {
+       React.createElement("img", {
           class: "imgclass",
           src: "./Assets/images/twitch.png",
           alt: "Youtube"
         })
       ),
-      /*#__PURE__*/ React.createElement(
+     React.createElement(
         "a",
         {
           href: "https://twitter.com/bracketproto"
         },
-        /*#__PURE__*/ React.createElement("img", {
+       React.createElement("img", {
           class: "imgclass",
           src: "./Assets/images/twitter.png",
           alt: "Youtube"
         })
       ),
-      /*#__PURE__*/ React.createElement(
+     React.createElement(
         "a",
         {
           href: "https://youtube.com/bracketproto"
         },
-        /*#__PURE__*/ React.createElement("img", {
+       React.createElement("img", {
           class: "imgclass",
           src: "./Assets/images/yt.png",
           alt: "Youtube"
         })
       ),
-      /*#__PURE__*/ React.createElement(
+     React.createElement(
         "a",
         {
           href: "https://discord.com/invite/9EWz4tekRR"
         },
-        /*#__PURE__*/ React.createElement("img", {
+       React.createElement("img", {
           class: "imgclass",
           src: "./Assets/images/discord.png",
           alt: "Youtube"
